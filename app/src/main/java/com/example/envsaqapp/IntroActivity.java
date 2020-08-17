2b44caef7b1731753cfdb88c5a4b8ab13525b79b
Mads Eibye <madseibye@gmail.com>
2020-08-17 11:09:51 +0200

    reee

--- app/src/main/java/com/example/envsaqapp/IntroActivity.java
+++ app/src/main/java/com/example/envsaqapp/IntroActivity.java
@@ -1,22 +1,32 @@
 package com.example.envsaqapp;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.ContentLoadingProgressBar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 
 public class IntroActivity extends AppCompatActivity {
 
     private static int TIME_OUT = 5000;
+    private ProgressBar progressBar;
+    private int mLoading = 0;
+    private Handler mHandler = new Handler();
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        new Handler().postDelayed(new Runnable() {
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+        textView = (TextView) findViewById(R.id.TextView);
+       /* new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent i = new Intent(IntroActivity.this, MainActivity.class);
@@ -25,5 +35,22 @@ public class IntroActivity extends AppCompatActivity {
                 finish();
             }
         }, TIME_OUT);
+*/
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (progressBar.getProgress() < 100) {
+                    mLoading++;
+                    progressBar.setProgress(mLoading);
+
+                }
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText("Loading Complete");
+                    }
+                });
+            }
+        }).start();
     }
-}
+};
