5ebf85ca0fbc4500555f7539a340727acb216c43
MadsEibye <madseibye96@gmail.com>
2020-08-19 10:36:37 -0700

    idk

--- app/src/main/java/com/example/envsaqapp/MainActivity.java
+++ app/src/main/java/com/example/envsaqapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.envsaqapp;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.PermissionChecker;
+
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -15,7 +17,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 import android.widget.Toast;
+
 import com.esri.arcgisruntime.geometry.Point;
 import com.esri.arcgisruntime.geometry.SpatialReferences;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
@@ -26,28 +30,36 @@ import com.esri.arcgisruntime.mapping.view.LatitudeLongitudeGrid;
 import com.esri.arcgisruntime.mapping.view.MapView;
 import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol;
 import com.google.android.gms.maps.OnMapReadyCallback;
+
 import java.security.Permission;
 import java.security.Permissions;
+
 public class MainActivity extends AppCompatActivity implements
         ActivityCompat.OnRequestPermissionsResultCallback {
     public ArcGISMap map;
-    private LocationManager LocationManager;
-    private LocationListener LocationListener;
     private MapView mapView;
-    public Location location;
-    public double Latitude = 40.75;
-    public double Longitude = -73.98;
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    protected Context context;
+    String lat;
+    String provider;
+    protected String latitude, longitude;
+    protected boolean gps_enabled, network_enabled;
+    public Location UserLocation;
+    public double Latitude = 55.6386;
+    public double Longitude = 12.0893;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private boolean permissionDenied = false;
     SimpleMarkerSymbol symbol;
     GraphicsOverlay graphicsOverlay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         MapView mapView = findViewById(R.id.MainMapView);
-        map = new ArcGISMap(Basemap.Type.STREETS, Latitude, Longitude, 16);
+        map = new ArcGISMap(Basemap.Type.STREETS, Latitude, Longitude, 14);
         mapView.setMap(map);
         GraphicsOverlay graphicsOverlay = new GraphicsOverlay();
         mapView.getGraphicsOverlays().add(graphicsOverlay);
@@ -56,12 +68,48 @@ public class MainActivity extends AppCompatActivity implements
         Point graphicPoint = new Point(Latitude, Longitude, SpatialReferences.getWebMercator());
         Graphic graphic = new Graphic(graphicPoint, symbol);
         graphicsOverlay.getGraphics().add(graphic);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                UserLocation = location;
+                //Latitude = location.getLatitude();
+                //Longitude = location.getLongitude();
+                Log.d("USERLOCATION", "" + location.getLatitude());
+                Log.d("USERLOCATION", "" + location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+                Latitude = UserLocation.getLatitude();
+                Longitude = UserLocation.getLongitude();
+                Log.d("USERLOCATION", "" + Latitude);
+                Log.d("USERLOCATION", "" + Longitude);
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+        };
         findLocation();
+
+
     }
+
+
     public void findLocation() {
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            Log.d("LOCATION", "" + Latitude);
-            Log.d("LOCATION", "" + Longitude);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            //Latitude = location.getLatitude();
+            //Longitude = location.getLongitude();
+            //getLocation();
+            Log.d("USERLOCATION", "" + Latitude);
+            Log.d("USERLOCATION", "" + Longitude);
         } else {
             if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
                 Toast.makeText(this, "You need too grant acess to your location", Toast.LENGTH_LONG).show();
@@ -69,37 +117,18 @@ public class MainActivity extends AppCompatActivity implements
             requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
         }
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
         if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
-                getLocation();
+                findLocation();
             } else {
                 Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
             }
         }
     }
-    public void getLocation(){
-        LocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        LocationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                Latitude = location.getLatitude();
-                Longitude = location.getLongitude();
-                Log.d("LOCATION","" + Latitude);
-                Log.d("LOCATION","" + Longitude);
-            }
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-            }
-            @Override
-            public void onProviderEnabled(String provider) {
-            }
-            @Override
-            public void onProviderDisabled(String provider) {
-            }
-        };
-    }
+
 }
