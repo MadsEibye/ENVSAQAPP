6ef54b270f4571450956f3cc137081cfaaa04768
MSI\Mathias <math662h@edu.easj.dk>
2020-08-20 13:21:30 +0200

    added intents + style
    added some cool features

---
+++ app/src/main/java/com/example/envsaqapp/ForureningHer.java
@@ -0,0 +1,125 @@
+package com.example.envsaqapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.net.sip.SipErrorCode.TIME_OUT;
+
+public class ForureningHer extends AppCompatActivity {
+    private static double userX;
+    private static double userY;
+    private TextView userLocationText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forurening_her);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Intent intent = getIntent();
+        userX = intent.getDoubleExtra("userX", userX);
+        userY = intent.getDoubleExtra("userY", userY);
+
+        userLocationText = findViewById(R.id.ForureningHerTextView);
+        userLocationText.setText("X " + userX + " " + "Y "  + userY);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+        return true;
+    }
+
+    public void ChangeActivity(Integer ID){
+        if (ID == item1ID){
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent i = new Intent(ForureningHer.this, ForureningHer.class);
+                    startActivity(i);
+                    overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+                    finish();
+                }
+            }, TIME_OUT);
+
+        }
+        else if (ID == item2ID){
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent i = new Intent(ForureningHer.this, MainActivity.class);
+                    startActivity(i);
+                    overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+                    finish();
+                }
+            }, TIME_OUT);
+        }
+        else if (ID == item3ID){
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent i = new Intent(ForureningHer.this, ForureningsUdsigt.class);
+                    startActivity(i);
+                    overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+                    finish();
+                }
+            }, TIME_OUT);
+        }
+        else if (ID == item4ID) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent i = new Intent(ForureningHer.this, Info.class);
+                    startActivity(i);
+                    overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+                    finish();
+                }
+            }, TIME_OUT);
+        }
+    }
+
+
+    private Integer item1ID;
+    private Integer item2ID;
+    private Integer item3ID;
+    private Integer item4ID;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                //Toast.makeText(this, "" + item.getItemId(), Toast.LENGTH_SHORT).show();
+                item1ID = item.getItemId();
+                ChangeActivity(item1ID);
+                return true;
+            case R.id.item2:
+                //Toast.makeText(this, "" + item.getItemId(), Toast.LENGTH_SHORT).show();
+                item2ID = item.getItemId();
+                ChangeActivity(item2ID);
+                return true;
+            case R.id.item3:
+                //Toast.makeText(this, "" + item.getItemId(), Toast.LENGTH_SHORT).show();
+                item3ID = item.getItemId();
+                ChangeActivity(item3ID);
+                return true;
+            case R.id.item4:
+                //Toast.makeText(this, "" + item.getItemId(), Toast.LENGTH_SHORT).show();
+                item4ID = item.getItemId();
+                ChangeActivity(item4ID);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
