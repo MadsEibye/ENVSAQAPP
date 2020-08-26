461b7c99244a96483782f005ab3b8a36ce823a26
math039h <43374961+math039h@users.noreply.github.com>
2020-08-26 12:31:12 +0200

    connection string

---
+++ app/src/main/java/com/example/envsaqapp/ConnectionString.java
@@ -0,0 +1,9 @@
+package com.example.envsaqapp;
+
+public class ConnectionString {
+    public static final String connectionString = "";
+
+    public ConnectionString(String connectionString) {
+    }
+
+}
