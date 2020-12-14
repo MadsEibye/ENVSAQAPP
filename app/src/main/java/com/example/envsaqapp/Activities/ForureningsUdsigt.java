09072cda6f0302fb761e6f462dc0f34653749274
MadsEibye <madseibye96@gmail.com>
2020-12-14 11:16:38 +0100

    Update ForureningsUdsigt.java

--- app/src/main/java/com/example/envsaqapp/Activities/ForureningsUdsigt.java
+++ app/src/main/java/com/example/envsaqapp/Activities/ForureningsUdsigt.java
@@ -525,18 +525,19 @@ public class ForureningsUdsigt extends AppCompatActivity implements NavigationVi
     private ArrayList<Entry> lineChartDataSet(ArrayList<ForureningsDataModel> dataList){
         ArrayList<Entry> dataSet = new ArrayList<Entry>();
         for (ForureningsDataModel o:dataList
-             ) { if (component == "PM10") {
-            dataSet.add(new Entry(o.getHour(), (float) o.getPM10()));
-        }
-        else if (component == "O3"){
-            dataSet.add(new Entry(o.getHour(), (float) o.getO3()));
-        }
-        else if (component == "PM2_5"){
-            dataSet.add(new Entry(o.getHour(), (float) o.getPM2_5()));
-        }
-        else {
-            dataSet.add(new Entry(o.getHour(), (float) o.getNo2()));
-        }
+             ) {
+            if (component.equals("No2")) {
+                dataSet.add(new Entry(o.getHour(), (float) o.getNo2()));
+            }
+            else if (component.equals("O3")){
+                dataSet.add(new Entry(o.getHour(), (float) o.getO3()));
+            }
+            else if (component.equals("PM2_5")){
+                dataSet.add(new Entry(o.getHour(), (float) o.getPM2_5()));
+            }
+            else {
+                dataSet.add(new Entry(o.getHour(), (float) o.getPM10()));
+            }
         }
         return dataSet;
     }
